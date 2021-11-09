a2ca10a99bf0a99e37cb772903d17348e521fb2d
pub <63778651+pubgyu@users.noreply.github.com>
2021-11-09 22:40:24 +0900

    test

--- js/pub-drive.js
+++ js/pub-drive.js
@@ -205,19 +205,15 @@ const starModel = (num) => {
 	}
 }
 
-let groundMaterial;
-let groundMaterial2;
 const groundModel = () => {
-	$scene.remove(groundMaterial);
-	$scene.remove(groundMaterial2);
 	// console.log('ground');
 	let groundGeo = new THREE.BoxGeometry(mapBlockW,mapBlockW,1);
-	groundMaterial = Physijs.createMaterial(
+	let groundMaterial = Physijs.createMaterial(
 		new THREE.MeshLambertMaterial({color: 0x3a3a3a, shininess: 150}),
 		5, // 높은 마찰계수
 		1 // 낮은 회복
 	);
-	groundMaterial2 = Physijs.createMaterial(
+	let groundMaterial2 = Physijs.createMaterial(
 		new THREE.MeshLambertMaterial({color: 0xe74f4f, shininess: 150}),
 		5, // 높은 마찰계수
 		1 // 낮은 회복
